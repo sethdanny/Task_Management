dc15bc156802e6935529bd396ca5154ce499e2aa
sethdanny <naddulidaniel1994@gmail.com>
2023-12-16 02:40:53 +0300

    created docker image

--- config/database.js
+++ config/database.js
@@ -5,10 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || 'mysql-db',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     charset: 'utf8mb4',
     waitForConnections: true,
   connectionLimit: 10,
